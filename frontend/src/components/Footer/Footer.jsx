2f506319293116c2430cb1404dad4a3c86e4624c
Lakshan Jayaweera <127861567+LakshanJayaweera426@users.noreply.github.com>
2024-07-09 14:38:13 +0900

    Developed Footer component and added css properties.

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+                <div className="footer-social-icon">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delievery</li>
+                    <li>Privacy police</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-212-456-7890</li>
+                    <li>contact@tomato.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className="copy-right">Copyright 2024 &copy; Tomato.com - All Right Reserved.</p>
+
+    </div>
+  )
+}
+
+export default Footer
